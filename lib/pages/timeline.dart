1264d4a582c4804792d9e59314ea0f8f8f4f5b97
Sagar <shreesagar48@gmail.com>
2020-05-01 19:57:01 +0545

    FutureBuilder for fetching data from database(not real time)

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -11,35 +11,49 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  List<dynamic> users = [];
+
   @override
   void initState() {
     super.initState();
-    getUsersById();
+    getUsers();
   }
 
-  // getUsers() {
-  //   userRef.getDocuments().then((QuerySnapshot snapshot) {
-  //     snapshot.documents.forEach((DocumentSnapshot doc) {
-  //       print(doc.data);
-  //       print(doc.documentID);
-  //       print(doc.exists);
-  //     });
-  //   });
+  getUsers() async {
+    final QuerySnapshot snapshot = await userRef.getDocuments();
+    setState(() {
+      users = snapshot.documents;
+    });
+  }
+
+  // getUsersById() async {
+  //   final String id = 'h2tcpe1jnOPs602iO5F2';
+  //   final DocumentSnapshot doc = await userRef.document(id).get();
+  //   print(doc.data);
+  //   print(doc.documentID);
+  //   print(doc.exists);
   // }
-
-  getUsersById() async {
-    final String id = 'h2tcpe1jnOPs602iO5F2';
-    final DocumentSnapshot doc = await userRef.document(id).get();
-    print(doc.data);
-    print(doc.documentID);
-    print(doc.exists);
-  }
 
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context),
-      body: circularProgress(),
+      body: FutureBuilder<QuerySnapshot>(
+        future: userRef.getDocuments(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Text> children = snapshot.data.documents
+              .map((doc) => Text(doc['username']))
+              .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 }
