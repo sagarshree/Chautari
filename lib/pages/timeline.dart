9a5cc0a712dccf34420074218bb8045a43ca77d0
Sagar <shreesagar48@gmail.com>
2020-05-01 19:58:07 +0545

    StreamBuilder for getting realtime data from database

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -38,8 +38,8 @@ class _TimelineState extends State<Timeline> {
   Widget build(context) {
     return Scaffold(
       appBar: header(context),
-      body: FutureBuilder<QuerySnapshot>(
-        future: userRef.getDocuments(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: userRef.snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return circularProgress();
